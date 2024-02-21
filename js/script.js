c387807bf3f4c95bbd01fa5265d67614cfd08794
jihyunkm <jihyunkim@berkeley.edu>
2024-02-21 10:47:37 -0800

    ss

--- js/script.js
+++ js/script.js
@@ -1,40 +1,42 @@
+
+
 // ********** INDEX **********
 
-function heroShow(imageID) {
-    document.getElementById(imageID).style.opacity = '1';
-}
+// function heroShow(imageID) {
+//     document.getElementById(imageID).style.opacity = '1';
+// }
 
-function heroHide(imageID) {
-    document.getElementById(imageID).style.opacity = '0';
-}
+// function heroHide(imageID) {
+//     document.getElementById(imageID).style.opacity = '0';
+// }
 
-// artist-turned hover
+// // artist-turned hover
 
-document.getElementById('artist-turned').addEventListener(
-    'mouseover', e => {heroShow('watercolor');}
-)
+// document.getElementById('artist-turned').addEventListener(
+//     'mouseover', e => {heroShow('watercolor');}
+// )
 
-document.getElementById('artist-turned').addEventListener(
-    'mouseout', e => {heroHide('watercolor');}
-)
+// document.getElementById('artist-turned').addEventListener(
+//     'mouseout', e => {heroHide('watercolor');}
+// )
 
-// based in california hover
+// // based in california hover
 
-document.getElementById('based-in').addEventListener(
-    'mouseover', e => {heroShow('california');}
-)
+// document.getElementById('based-in').addEventListener(
+//     'mouseover', e => {heroShow('california');}
+// )
 
-document.getElementById('based-in').addEventListener(
-    'mouseout', e => {heroHide('california');}
-)
+// document.getElementById('based-in').addEventListener(
+//     'mouseout', e => {heroHide('california');}
+// )
 
-// product designer hover
+// // product designer hover
 
-document.getElementById('designed-for').addEventListener(
-    'mouseover', e => {heroShow('scroll');}
-)
+// document.getElementById('designed-for').addEventListener(
+//     'mouseover', e => {heroShow('scroll');}
+// )
 
-document.getElementById('designed-for').addEventListener(
-    'mouseout', e => {heroHide('scroll');}
-)
+// document.getElementById('designed-for').addEventListener(
+//     'mouseout', e => {heroHide('scroll');}
+// )
 
